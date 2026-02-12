3c02629ef86e33178a0b07aa7edd8e8775499296
Joey <39647@ma-web.nl>
2026-02-12 15:08:29 +0100

    3.1 version push (no grid based movement yet)

--- m3-pacman/Assets/Scripts/PlayerMovement.cs
+++ m3-pacman/Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rb;
     private Vector2 movement;
+    private Vector2 movementDirection;
 
     void Start()
     {
@@ -13,21 +15,57 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        // Get input
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            movementDirection = Vector2.up;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            movementDirection = Vector2.down;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            movementDirection = Vector2.left;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            movementDirection = Vector2.right;
     }
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-    }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
+        void FixedUpdate()
         {
-            Destroy(gameObject);
-            Debug.Log("Dot collected!");
+            // changing direction 
+            if (CanMove(movementDirection))
+            {
+                movement = movementDirection;
+            }
+
+            // keeping direction
+            if (CanMove(movement))
+            {
+                rb.linearVelocity = movement * speed;
+            }
+            else
+            {
+                rb.linearVelocity = Vector2.zero;
+            }
+        }
+
+        bool CanMove(Vector2 direction)
+        {
+            if (direction == Vector2.zero)
+                return false;
+
+            RaycastHit2D hit = Physics2D.Raycast(
+                transform.position,
+                direction,
+                0.55f
+            );
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.CompareTag("Wall"))
+                    return false;
+            }
+
+            return true;
         }
     }
-}
